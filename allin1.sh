41359c4c70b4486b4cc7c06fe529a2f21b44f7c2
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-17 00:27:17 +0530

    All in 1

---
+++ allin1.sh
@@ -0,0 +1,15 @@
+sudo apt-get update;
+sudo apt-get install vim;
+sudo apt-get update;
+sudo add-apt-repository ppa:webupd8team/sublime-text-2;
+sudo apt-get update;sudo apt-get install sublime-text; 
+sudo add-apt-repository ppa:umang/indicator-stickynotes;
+sudo apt-get update;
+sudo apt-get install indicator-stickynotes;
+sudo apt-get install tasksel; 
+sudo tasksel;
+sudo apt-get install phpmyadmin
+sudo add-apt-repository ppa:webupd8team/java;
+sudo apt-get update;
+sudo apt-get install oracle-java8-installer;
+sudo apt-get install git-flow;
