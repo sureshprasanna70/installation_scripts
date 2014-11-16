ecc8162b87315d18e6353cabdede168a038d3936
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-17 00:25:14 +0530

    Bison removed

--- ror.sh
+++ ror.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison nodejs
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool nodejs
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz
 tar -zxf ruby-2.1.4.tar.gz
 cd ruby 2.1.4
