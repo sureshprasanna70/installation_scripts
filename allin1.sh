ff11558e085e32a798523bd29ebb4b1bbc888791
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-17 11:36:28 +0530

    Moved git installation from ror file to allin1

--- allin1.sh
+++ allin1.sh
@@ -12,4 +12,4 @@ sudo apt-get install phpmyadmin
 sudo add-apt-repository ppa:webupd8team/java;
 sudo apt-get update;
 sudo apt-get install oracle-java8-installer;
-sudo apt-get install git-flow;
+sudo apt-get install git-core git-flow;
