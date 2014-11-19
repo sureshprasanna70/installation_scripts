83b9e9c098b9c08f775ee73db242603362f2cd10
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 20:11:01 +0530

    Moved nodejs to chrislea ppa

--- allin1.sh
+++ allin1.sh
@@ -13,3 +13,6 @@ sudo add-apt-repository ppa:webupd8team/java;
 sudo apt-get update;
 sudo apt-get install oracle-java8-installer;
 sudo apt-get install git-core git-flow;
+sudo add-apt-repository ppa:chris-lea/node.js  
+sudo apt-get update  
+sudo apt-get install nodejs
